0c6536d79c6cb46271a8255c25993c76ad4813ba
THE-ANUBHAV <anubhavsingh14122005@gmail.com>
2025-08-15 20:59:22 +0530

    [ANUBHAV SINGH] Added PerimeterofSquare.java Program

---
+++ LEVEL-1 Solutions/PerimeterofSquare.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Take perimeter input from user
+        System.out.print("Enter the perimeter of the square: ");
+        double perimeter = input.nextDouble();
+
+        // Calculate side (Perimeter = 4 × side)
+        double side = perimeter / 4;
+
+        // Output
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
