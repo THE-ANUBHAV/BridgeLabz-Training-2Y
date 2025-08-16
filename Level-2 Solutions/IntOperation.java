de2856d2cd90cf371be4f23a873d1664e78bbfc6
THE-ANUBHAV <anubhavsingh14122005@gmail.com>
2025-08-16 11:47:16 +0530

    [ANUBHAV SINGH] Added IntOperation.java Program

---
+++ Level-2 Solutions/IntOperation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IntOperation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input values
+        System.out.print("Enter value of a: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter value of b: ");
+        int b = input.nextInt();
+
+        System.out.print("Enter value of c: ");
+        int c = input.nextInt();
+
+        // Perform operations (conside
+
